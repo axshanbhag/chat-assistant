ff796e8172b1d676789d0326f2e60a5235d3d27d
axshanbhag <axshanbhag@zonemail.clpccd.edu>
2025-09-01 13:49:45 -0700

    intial commit

---
+++ voice_assistant.py
@@ -0,0 +1,59 @@
+import os
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs.conversational_ai.conversation import Conversation
+from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
+from elevenlabs.types import ConversationConfig
+
+load_dotenv()
+
+AGENT_ID = os.getenv("AGENT_ID")
+API_KEY = os.getenv("API_KEY")
+
+user_name = "Arpita"
+schedule = "School from 8AM to 2PM, Meeting from 6PM to 7PM"
+prompt = f"You are a helpful assistant. Your interlocutor has the following schedule: {schedule}."
+first_message = f"Hello {user_name}, how can I help you today?"
+
+conversation_override = {
+    "agent": {
+        "prompt": {
+            "prompt": prompt,
+        },
+        "first_message": first_message,
+    },
+}
+
+config = ConversationConfig(
+    conversation_config_override=conversation_override,
+    extra_body={},
+    dynamic_variables={},
+    user_id="user_12345",
+    max_response_tokens=8000,
+    interrupt_on_long_response=False,
+)
+
+def print_agent_response(response):
+    print(f"Agent: {response}")
+
+def print_interrupted_response(original, corrected):
+    pass
+    #print(f"Agent interrupted, truncated response: {corrected}")
+
+def print_user_transcript(transcript):
+    print(f"User: {transcript}")
+
+client = ElevenLabs(api_key=API_KEY)
+
+conversation = Conversation(
+    client,
+    AGENT_ID,
+    config=config,
+    requires_auth=True,
+    audio_interface=DefaultAudioInterface(),
+    callback_agent_response=print_agent_response,
+    callback_agent_response_correction=print_interrupted_response,
+    callback_user_transcript=print_user_transcript,
+)
+
+conversation.start_session()
